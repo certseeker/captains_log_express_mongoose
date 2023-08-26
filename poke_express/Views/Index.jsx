b8964c3805caa9a01fdbd1bbbed3c85b7b79ca3f
CertSeeker <133781991+certseeker@users.noreply.github.com>
2023-08-25 23:15:25 -0400

    Second Commit

---
+++ poke_express/Views/Index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Index({pokemon}) {
+
+  const overallStyle = {
+    color: ""
+
+  }
+
+
+  return(
+    <div style={overallStyle}>
+      <h1>See All That Pokemon!</h1>
+        <ul>
+          
+        </ul>
+
+
+    </div>
+
+  )
+
+}
--- poke_express/server.js
+++ poke_express/server.js
@@ -8,11 +8,18 @@ app.get("/" , (req , res) => {
   res.send("<h1>Welcome to the Pokemon App!</h1>")
 })
 
+const pokemon = require('./pokemon.js')
 
-app.get("/pokemon" , (req, res) => {
-  res.render(
-  )
-})
+// just showing the data in json on the browser
+// app.get("/pokemon" , (req, res) => {
+//   res.send(pokemon)
+// })
+
+app.get('pokemon' , (req, res) => {
+  res.render("pokemon" , {
+    pokemon : pokemon
+  });
+});
 
 app.listen(3000, () => {
   console.log("Listening on Port 3000")
