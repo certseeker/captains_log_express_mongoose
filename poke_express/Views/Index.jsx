abb301d8341c6d0ec138dff28c3d45b25b55de6c
CertSeeker <133781991+certseeker@users.noreply.github.com>
2023-08-26 13:09:13 -0400

    third commit

--- poke_express/Views/Index.jsx
+++ poke_express/Views/Index.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 
+// passing pokemon in a
 function Index({pokemon}) {
 
-  const overallStyle = {
-    color: ""
+  // const overallStyle = {
+  //   color: ""
 
-  }
-
+  // }
+  // style={overallStyle}
 
   return(
-    <div style={overallStyle}>
+    <div>
       <h1>See All That Pokemon!</h1>
         <ul>
-          
+          {pokemon.map((pokelist, i) => {
+            return (
+              // {pokelist}
+              <li key={i}>
+
+                <a href={`/pokemon/${pokelist.name}`}>{pokelist.name}</a>
+                {/* // this is if the images are on the main page */}
+                {/* <img src={`${pokelist.img}.jpg`} alt={pokelist.name} /> */}
+              </li>
+            )
+          })}
         </ul>
-
-
     </div>
 
   )
-
 }
+module.exports = Index;
--- poke_express/server.js
+++ poke_express/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
 const app = express();
+//!this allows express/node to read the jsx files, if you don't this and have jsx, it will cause and error and not render page
+app.set('view engine', "jsx");
+
+//!DO NOT FORGET to put the () after createEngine !!!
+app.engine('jsx' , require('express-react-views').createEngine());
+
+const pokemon = require('./Models/pokemon.js')
 
 // INDECES
 
@@ -8,15 +14,17 @@ app.get("/" , (req , res) => {
   res.send("<h1>Welcome to the Pokemon App!</h1>")
 })
 
-const pokemon = require('./pokemon.js')
+
 
 // just showing the data in json on the browser
 // app.get("/pokemon" , (req, res) => {
 //   res.send(pokemon)
 // })
 
-app.get('pokemon' , (req, res) => {
-  res.render("pokemon" , {
+    //the first pokemon is what I am naming rn and the second refers to the item on the index list
+app.get('/pokemon' , (req, res) => {
+  // console.log(pokemon)
+  res.render("Index" , {
     pokemon : pokemon
   });
 });
